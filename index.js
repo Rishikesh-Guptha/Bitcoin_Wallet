fec180723db2bd7fcb2123c0592000a2015f1c25
Rishikesh-Guptha <rishikeshrishi2004@gmail.com>
2023-06-26 18:19:45 +0530

    added the code for the private key and the wallet address

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function clicked(){
+    //To get Private Key
     var p_key= bsv.PrivateKey.fromRandom();
     var pr_key=p_key.toString();
     document.getElementById("private-key").innerHTML=pr_key;
+
+    // We get the public key from the private key
+    var pub_key =bsv.PublicKey.fromPrivateKey(p_key);
+    var public_key= pub_key.toString();
+    document.getElementById("public-key").innerHTML=public_key;
+
+    var wallet_address = bsv.Address.fromPublicKey(pub_key);
+    var wall_address= wallet_address.toString();
+    document.getElementById("address").innerHTML=wall_address;
 }
 
  
